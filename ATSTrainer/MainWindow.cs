bdd52d40e9182153bd852bd1b861ed2d5c1b19fc
Saniee <asamsku10@gmail.com>
2021-08-05 01:42:05 +0200

    Initial Code

---
+++ ATSTrainer/MainWindow.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+
+namespace ATSTrainer
+{
+    public partial class MainWindow : Form
+    {
+        Mem m = new Mem();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_Shown(object sender, EventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        bool ProcOpen = false;
+
+        private void BGWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            ProcOpen = m.OpenProcess("amtrucks");
+
+            Thread.Sleep(100);
+            BGWorker.ReportProgress(0);
+        }
+
+        private void BGWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (ProcOpen)
+            {
+                ProcOpenLabel.ForeColor = Color.Green;
+                ProcOpenLabel.Text = "Yes";
+            } 
+            else
+            {
+                ProcOpenLabel.ForeColor = Color.Red;
+                ProcOpenLabel.Text = "N/A";
+                return;
+            }
+        }
+
+        private void BGWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            BGWorker.RunWorkerAsync();
+        }
+
+        private void SetMoney_Click(object sender, EventArgs e)
+        {
+            if (MoneyAmount.Text == "")
+            {
+                MoneyAmount.Text = "Type in a number.";
+            }
+            else
+            {
+                // Public Beta 1.41
+                m.WriteMemory("amtrucks.exe+01931738,10,10", "int", MoneyAmount.Text);
+            }
+        }
+
+        private void SetEXP_Click(object sender, EventArgs e)
+        {
+            if (EXPAmount.Text == "")
+            {
+                EXPAmount.Text = "Type in a number.";
+            }
+            else
+            {
+                // Public Beta 1.41
+                m.WriteMemory("amtrucks.exe+01931738,195C", "int", EXPAmount.Text);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ATSTrainer
+# ATS Trainer
+# The Trainer needs a Admin privilages!
+
+Best used for editing your profile for more money and exp so you dont have to grind.
+
+# Tag for releases: 
+If it has 'B' on the end it means its a beta version of the game. <br />
+If it has 'R' on the end it means its the latest stable version of the game. <br />
+If it has 'E' on the end it means its the experimental version of the game.
+
+![ets2trainer](https://i.imgur.com/jhIWixk.png)
